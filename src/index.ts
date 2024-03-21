bed3750fdef29175dd1fd19f6c79d5c4dec8be46
aleksandr_zelenskiy <dezion777@gmail.com>
2024-03-21 12:26:35 +0300

    BP-3 webpack dev server, env

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import {some} from './test'
 
 some(5)
+
+document.body.innerHTML = `<div>Hello World</div>`
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,22 +1,27 @@
 import path from 'path';
 import webpack from 'webpack';
 import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
-import {BuildPaths} from "./config/build/types/config";
+import {BuildEnv, BuildPaths} from "./config/build/types/config";
 
-const paths: BuildPaths = {
-    html: path.resolve(__dirname, 'public', 'index.html'),
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    build: path.resolve(__dirname, 'build')
+
+export default (env: BuildEnv) => {
+    const paths: BuildPaths = {
+        html: path.resolve(__dirname, 'public', 'index.html'),
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        build: path.resolve(__dirname, 'build')
+    }
+
+    const mode = env.mode || 'development'
+    const PORT = env.port || 3001;
+
+    const isDev: boolean = mode === 'development'
+
+    const config: webpack.Configuration = buildWebpackConfig({
+        mode: mode,
+        paths: paths,
+        isDev,
+        port: PORT,
+    })
+
+    return config
 }
-
-const mode = 'development'
-
-const isDev: boolean = mode === 'development'
-
-const config: webpack.Configuration = buildWebpackConfig({
-    mode: "development",
-    paths: paths,
-    isDev,
-})
-
-export default config;
